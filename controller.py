0a2ce59325556cd62e9d1ddf7ddf27b7dc612df3
Jeremi Herodian Abednigo <Overload.emperor14@gmail.com>
2023-05-24 12:40:49 +0800

    Update cors and inference

--- controller.py
+++ controller.py
@@ -1,34 +1,32 @@
 from fastapi import UploadFile
 
 import tensorflow as tf
-from keras.utils import load_img, img_to_array
+from keras.utils import img_to_array
 import numpy as np
 from PIL import Image
 
 def preprocessing(fileImage: UploadFile):
     img = Image.open(fileImage.file).resize((224, 224))
     img_array = img_to_array(img)
-    img_preprocessed = tf.keras.applications.resnet50.preprocess_input(np.expand_dims(img_array, axis=0))
+    img_preprocessed = np.expand_dims(img_array, axis=0)
     return img_preprocessed
 
 def post_preprocessing(img_postpreprocessing):
-    model = tf.keras.models.load_model('./models/softmax_new_model.h5')
-    predictions = model.predict(img_postpreprocessing)
-    predicted_class = np.argmax(predictions, axis=1)[0]
-    class_labels = ['fresh', 'spoiled']
-    predicted_label = class_labels[predicted_class]
-    predicted_probability = predictions[0][predicted_class] * 100
-    return predicted_label, predicted_probability
+    model = tf.keras.models.load_model('./models/beefy-mobilenetv2.h5')
+    predictions = model.predict(img_postpreprocessing).flatten()
+    predictions = tf.nn.sigmoid(img_postpreprocessing)
+    predicted_label = tf.where(predictions < 0.5, 'fresh', 'spoiled').numpy()[0]
+    return predicted_label, predictions
 
 
-def predictMeat(file: UploadFile):
+def inference(file: UploadFile):
     try:
         image = preprocessing(fileImage=file)
         label, probability = post_preprocessing(img_postpreprocessing=image)
-        response = {
+        responseBody = {
             'label': label,
             'pobability': probability
         }
-        return (True, response)
+        return True, responseBody
     except:
-        return (False, None)
+        return False, None
