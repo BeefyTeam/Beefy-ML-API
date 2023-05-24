79afdbc0c81df5eb174ae16609f463487487eba0
Jeremi Herodian Abednigo <Overload.emperor14@gmail.com>
2023-05-24 13:42:11 +0800

    Fix code

--- controller.py
+++ controller.py
@@ -1,31 +1,32 @@
 from fastapi import UploadFile
 
 import tensorflow as tf
+from keras.models import load_model
 from keras.utils import img_to_array
 import numpy as np
 from PIL import Image
 
 def preprocessing(fileImage: UploadFile):
-    img = Image.open(fileImage.file).resize((224, 224))
-    img_array = img_to_array(img)
+    img = Image.open(fileImage.file)
+    img = img.resize((224, 224))
+    img_array = img_to_array(img, dtype=np.float32)
     img_preprocessed = np.expand_dims(img_array, axis=0)
     return img_preprocessed
 
 def post_preprocessing(img_postpreprocessing):
-    model = tf.keras.models.load_model('./models/beefy-mobilenetv2.h5')
+    model = load_model('./models/beefy-mobilenetv2.h5')
     predictions = model.predict(img_postpreprocessing).flatten()
-    predictions = tf.nn.sigmoid(img_postpreprocessing)
+    predictions = tf.nn.sigmoid(predictions, name='sigmoid')
     predicted_label = tf.where(predictions < 0.5, 'fresh', 'spoiled').numpy()[0]
-    return predicted_label, predictions
-
+    label_final = bytes.decode(predicted_label, 'utf-8')
+    return label_final
 
 def inference(file: UploadFile):
     try:
         image = preprocessing(fileImage=file)
-        label, probability = post_preprocessing(img_postpreprocessing=image)
+        label = post_preprocessing(img_postpreprocessing=image)
         responseBody = {
             'label': label,
-            'pobability': probability
         }
         return True, responseBody
     except:
